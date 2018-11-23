1fdd95237a3467b6c4d5396feff30eef09c425f7
Victor Feitosa <victor.klngo@gmail.com>
2018-11-22 23:00:14 -0300

    added gastby publishing

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ import Image from '../components/image'
 const IndexPage = () => (
   <Layout>
     <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
+    <p>New Kalango homepage</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
       <Image />
