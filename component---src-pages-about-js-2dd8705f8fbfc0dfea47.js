8d4202aa40aa338fbb108c3b2feef7b7ccf348af
Victor Feitosa <victor.klngo@gmail.com>
2019-01-04 13:19:44 -0300

    Updates

---
+++ component---src-pages-about-js-2dd8705f8fbfc0dfea47.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{156:function(n,e,t){"use strict";t.r(e);var u=t(0),o=t.n(u);e.default=function(){return o.a.createElement("div",null," About ")}}}]);
+//# sourceMappingURL=component---src-pages-about-js-2dd8705f8fbfc0dfea47.js.map
