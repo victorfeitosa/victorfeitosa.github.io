9169a50baa48087bca92f6e2904929b6f9679ddb
Victor Feitosa <victor.klngo@gmail.com>
2019-01-22 23:03:26 -0300

    small fixes and added icon

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -9,76 +9,61 @@ import { navigate, globalHistory } from '@reach/router'
 import '../styles/Layout.scss'
 
 export class Layout extends React.Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      layoutRendered: false
-    }
-  }
-
-  componentDidMount() {
-    if(!this.state.layoutRendered) {
-      this.setState({ layoutRendered: true })
-    }
-  }
-
   render() {
     const outsideHome = globalHistory.location.pathname !== '/'
-    const layoutRendered = this.state.layoutRendered
     const children = this.props.children
 
     return (
       <div>
-        <Helmet
-          title="Victor Feitosa Web Portfolio"
-          meta={[
-            {
-              name: 'description',
-              content: 'Victor Feitosa Portfolio Website',
-            },
-            {
-              name: 'keywords',
-              content:
-                'victor, feitosa, victorfeitosa, porftolio, front, end, frontend, front-end, development, design, webdesign, web-design',
-            },
-          ]}
-        >
-          <html lang="en" />
-        </Helmet>
-        <div className='main-layout'>
-          {/* NOTE: Purple section containing profile */}
-          <section
-            onClick={() => {
-              navigate('/')
-            }}
-            className={
-              'profile-section' +
-              (outsideHome ? ' profile-section--open' : '')
-            }
+          <Helmet
+            title="Victor Feitosa Web Portfolio"
+            meta={[
+              {
+                name: 'description',
+                content: 'Victor Feitosa Portfolio Website',
+              },
+              {
+                name: 'keywords',
+                content:
+                  'victor, feitosa, victorfeitosa, porftolio, front, end, frontend, front-end, development, design, webdesign, web-design',
+              },
+            ]}
           >
-            <BigPic isSmall={outsideHome} src="" />
+            <html lang="en" />
+          </Helmet>
+            <div className='main-layout'>
+            {/* NOTE: Purple section containing profile */}
+            <section
+              onClick={() => {
+                navigate('/')
+              }}
+              className={
+                'profile-section' +
+                (outsideHome ? ' profile-section--open' : '')
+              }
+            >
+              <BigPic isSmall={outsideHome} src="" />
 
-            <div className={'info-block' + (outsideHome ? ' info-block--small' : '')} >
-              <SubInfo isSmall={outsideHome} />
-            </div>
-          </section>
+              <div className={'info-block' + (outsideHome ? ' info-block--small' : '')} >
+                <SubInfo isSmall={outsideHome} />
+              </div>
+            </section>
 
-          {/* NOTE: Page section where pages are loaded */}
-          <section className={
-            'content-section' +
-            (outsideHome ? ' content-section--open' : '')
-          }>
-            {layoutRendered ? children : ''}
-          </section>
+            {/* NOTE: Page section where pages are loaded */}
+            <section className={
+              'content-section' +
+              (outsideHome ? ' content-section--open' : '')
+            }>
+              { children }
+            </section>
 
-          {/* NOTE: Menu section */}
-          <section className='menu-section'>
-            <Menu />
-            <Copyright />
-          </section>
+            {/* NOTE: Menu section */}
+            <section className='menu-section'>
+              <Menu />
+              <Copyright />
+            </section>
+          </div>
         </div>
-      </div>
     )
   }
 }
--- todo.md
+++ todo.md
@@ -1,9 +1,7 @@
-+ Finish website pages while open
 + Add integratopm with google docs viewer
   + Add button to download as PDF
 + Add projects page and content
 + Finish website content
 + Cleanup
-  + Swap layout plugin with component implementation
   + Remove unused libraries such as redux
 + Add blog?
