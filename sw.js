4a855ab7afc079b92cc630b0df323b46d390da45
Victor Feitosa <victor.klngo@gmail.com>
2019-03-17 14:20:01 -0300

    Updates

--- sw.js
+++ sw.js
@@ -29,7 +29,7 @@ self.__precacheManifest = [
     "url": "webpack-runtime-6f9540ad8ac9d017c965.js"
   },
   {
-    "url": "app.6f58e31bf3baaf84c1c8.css"
+    "url": "app.f695df92164a6117e904.css"
   },
   {
     "url": "app-baaf0ef2db80694eb4b1.js"
@@ -39,7 +39,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "1d5d4a180b05ee758091f51d92c834da"
+    "revision": "75cf093da7e798e5fb25e56b6197769c"
   },
   {
     "url": "component---src-pages-404-js-8fd0f2e00fdcb34be107.js"
