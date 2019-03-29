71a3a170f692d060963428f315b4352fb53232ba
Victor Feitosa <victor.ramon.maia.feitosa@intrapoint.com>
2019-03-29 15:59:22 -0300

    partially solving breaking issues in styles

--- src/components/BigPic.js
+++ src/components/BigPic.js
@@ -2,7 +2,9 @@ import React from 'react'
 
 const BigPic = (props) => {
   return (
-    <div className={'big-pic' + (props.isSmall ? ' big-pic--small' : '')}></div>
+    <div className='big-pic-container'>
+      <div className={'big-pic' + (props.isSmall ? ' big-pic--small' : '')}></div>
+    </div>
   )
 }
 
