6d83f67c3e0e62c005e3500908760cc4a8272549
Victor Feitosa <victor.f@getjobber.com>
2020-03-27 13:07:44 -0400

    change default pwa name

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,7 +18,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'gatsby-starter-default',
+        name: 'victorfeitosa-website',
         short_name: 'starter',
         start_url: '/',
         background_color: '#663399',
